19485dbca5bc91a3d08dcf0137e4e2787ef422a6
tobiasherzke <tobiasherzke@openmha.com>
2021-06-29 10:43:20 +0200

    Add test for loguru thread local storage

--- testing/test_int_streaminfo.cpp
+++ testing/test_int_streaminfo.cpp
@@ -1,8 +1,21 @@
 #include "../src/api_config.h"
 #include "../src/stream_info_impl.h"
 #include <loguru.hpp>
+#include <thread>
 
 #include <catch2/catch.hpp>
+
+TEST_CASE("loguru_thread_local_storage", "[threading]") {
+	char name[2] = "0";
+	loguru::set_thread_name("1");
+	std::thread([&](){
+		loguru::set_thread_name("2");
+		loguru::get_thread_name(name,2,false);
+	}).join();
+	REQUIRE(name[0] == '2');
+	loguru::get_thread_name(name,2,false);
+	REQUIRE(name[0] == '1');
+}
 
 TEST_CASE("streaminfo matching via XPath", "[basic][streaminfo][xml]") {
 	lsl::stream_info_impl info(
